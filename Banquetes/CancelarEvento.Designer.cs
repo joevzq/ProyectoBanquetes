ed67ea8a58ac2efd53306bc2b29fd8d3aba12068
joevzq <joevzq@gmail.com>
2015-05-30 00:10:20 -0500

    Pantalla Cancelar Evento

---
+++ Banquetes/CancelarEvento.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Banquetes
+{
+    partial class CancelarEvento
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CancelarEvento));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblCancelar = new System.Windows.Forms.Label();
+            this.lblSeguro = new System.Windows.Forms.Label();
+            this.lblComentario = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSi = new System.Windows.Forms.Button();
+            this.btnNo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(272, 40);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(44, 28);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // lblCancelar
+            // 
+            this.lblCancelar.AutoSize = true;
+            this.lblCancelar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(88)))), ((int)(((byte)(70)))));
+            this.lblCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCancelar.ForeColor = System.Drawing.Color.White;
+            this.lblCancelar.Location = new System.Drawing.Point(12, 9);
+            this.lblCancelar.Name = "lblCancelar";
+            this.lblCancelar.Size = new System.Drawing.Size(132, 18);
+            this.lblCancelar.TabIndex = 0;
+            this.lblCancelar.Text = "Cancelar Evento";
+            // 
+            // lblSeguro
+            // 
+            this.lblSeguro.AutoSize = true;
+            this.lblSeguro.Location = new System.Drawing.Point(12, 36);
+            this.lblSeguro.Name = "lblSeguro";
+            this.lblSeguro.Size = new System.Drawing.Size(214, 13);
+            this.lblSeguro.TabIndex = 1;
+            this.lblSeguro.Text = "¿Seguro que deseas cancelar este evento?";
+            // 
+            // lblComentario
+            // 
+            this.lblComentario.AutoSize = true;
+            this.lblComentario.Location = new System.Drawing.Point(12, 58);
+            this.lblComentario.Name = "lblComentario";
+            this.lblComentario.Size = new System.Drawing.Size(63, 13);
+            this.lblComentario.TabIndex = 2;
+            this.lblComentario.Text = "Comentario:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 74);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(286, 110);
+            this.textBox1.TabIndex = 3;
+            // 
+            // btnSi
+            // 
+            this.btnSi.Location = new System.Drawing.Point(223, 194);
+            this.btnSi.Name = "btnSi";
+            this.btnSi.Size = new System.Drawing.Size(75, 23);
+            this.btnSi.TabIndex = 4;
+            this.btnSi.Text = "Si";
+            this.btnSi.UseVisualStyleBackColor = true;
+            // 
+            // btnNo
+            // 
+            this.btnNo.Location = new System.Drawing.Point(15, 194);
+            this.btnNo.Name = "btnNo";
+            this.btnNo.Size = new System.Drawing.Size(75, 23);
+            this.btnNo.TabIndex = 5;
+            this.btnNo.Text = "No";
+            this.btnNo.UseVisualStyleBackColor = true;
+            // 
+            // CancelarEvento
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(313, 231);
+            this.Controls.Add(this.btnNo);
+            this.Controls.Add(this.btnSi);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblComentario);
+            this.Controls.Add(this.lblSeguro);
+            this.Controls.Add(this.lblCancelar);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "CancelarEvento";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cancelar Evento";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblCancelar;
+        private System.Windows.Forms.Label lblSeguro;
+        private System.Windows.Forms.Label lblComentario;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnSi;
+        private System.Windows.Forms.Button btnNo;
+    }
+}
---
+++ Banquetes/CancelarEvento.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Banquetes
+{
+    public partial class CancelarEvento : Form
+    {
+        public CancelarEvento()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(System.Drawing.ColorTranslator.FromHtml("#D85846"));
+            System.Drawing.Graphics formGraphics;
+            formGraphics = this.CreateGraphics();
+            formGraphics.FillRectangle(myBrush, new Rectangle(5, 9, 300, 20));
+            myBrush.Dispose();
+            formGraphics.Dispose();
+        }
+    }
+}
