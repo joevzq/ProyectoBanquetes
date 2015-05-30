455b69056d1c84c70205144a0f6f430a439a7367
joevzq <joevzq@gmail.com>
2015-05-30 17:08:36 -0500

    Actualizacion ventana de Evento

--- Banquetes/Cliente.Designer.cs
+++ Banquetes/Cliente.Designer.cs
@@ -29,46 +29,69 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Cliente));
-            this.grbOne = new System.Windows.Forms.GroupBox();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.txtTel = new System.Windows.Forms.TextBox();
+            this.txtMaterno = new System.Windows.Forms.TextBox();
+            this.txtPaterno = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblMaterno = new System.Windows.Forms.Label();
             this.lblMail = new System.Windows.Forms.Label();
             this.lblTelefono = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.lblPaterno = new System.Windows.Forms.Label();
             this.lblNombre = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.lblTitle = new System.Windows.Forms.Label();
             this.btnAnterior = new System.Windows.Forms.Button();
             this.btnSiguiente = new System.Windows.Forms.Button();
-            this.lblMaterno = new System.Windows.Forms.Label();
-            this.txtNombre = new System.Windows.Forms.TextBox();
-            this.txtPaterno = new System.Windows.Forms.TextBox();
-            this.txtMaterno = new System.Windows.Forms.TextBox();
-            this.txtTel = new System.Windows.Forms.TextBox();
-            this.txtMail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.grbOne = new System.Windows.Forms.GroupBox();
             this.grbOne.SuspendLayout();
             this.SuspendLayout();
             // 
-            // grbOne
+            // txtMail
             // 
-            this.grbOne.BackColor = System.Drawing.Color.White;
-            this.grbOne.Controls.Add(this.txtMail);
-            this.grbOne.Controls.Add(this.txtTel);
-            this.grbOne.Controls.Add(this.txtMaterno);
-            this.grbOne.Controls.Add(this.txtPaterno);
-            this.grbOne.Controls.Add(this.txtNombre);
-            this.grbOne.Controls.Add(this.lblMaterno);
-            this.grbOne.Controls.Add(this.lblMail);
-            this.grbOne.Controls.Add(this.lblTelefono);
-            this.grbOne.Controls.Add(this.label3);
-            this.grbOne.Controls.Add(this.lblPaterno);
-            this.grbOne.Controls.Add(this.lblNombre);
-            this.grbOne.Controls.Add(this.label1);
-            this.grbOne.Location = new System.Drawing.Point(13, 45);
-            this.grbOne.Name = "grbOne";
-            this.grbOne.Size = new System.Drawing.Size(254, 166);
-            this.grbOne.TabIndex = 0;
-            this.grbOne.TabStop = false;
+            this.txtMail.Location = new System.Drawing.Point(139, 127);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(100, 20);
+            this.txtMail.TabIndex = 20;
+            // 
+            // txtTel
+            // 
+            this.txtTel.Location = new System.Drawing.Point(139, 101);
+            this.txtTel.Name = "txtTel";
+            this.txtTel.Size = new System.Drawing.Size(100, 20);
+            this.txtTel.TabIndex = 19;
+            // 
+            // txtMaterno
+            // 
+            this.txtMaterno.Location = new System.Drawing.Point(139, 75);
+            this.txtMaterno.Name = "txtMaterno";
+            this.txtMaterno.Size = new System.Drawing.Size(100, 20);
+            this.txtMaterno.TabIndex = 18;
+            // 
+            // txtPaterno
+            // 
+            this.txtPaterno.Location = new System.Drawing.Point(139, 49);
+            this.txtPaterno.Name = "txtPaterno";
+            this.txtPaterno.Size = new System.Drawing.Size(100, 20);
+            this.txtPaterno.TabIndex = 17;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(139, 23);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 16;
+            // 
+            // lblMaterno
+            // 
+            this.lblMaterno.AutoSize = true;
+            this.lblMaterno.Location = new System.Drawing.Point(13, 78);
+            this.lblMaterno.Name = "lblMaterno";
+            this.lblMaterno.Size = new System.Drawing.Size(59, 13);
+            this.lblMaterno.TabIndex = 15;
+            this.lblMaterno.Text = "Apellido M.";
             // 
             // lblMail
             // 
@@ -92,16 +115,6 @@
             this.lblTelefono.TabIndex = 13;
             this.lblTelefono.Text = "Teléfono";
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(13, 90);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(0, 15);
-            this.label3.TabIndex = 12;
-            // 
             // lblPaterno
             // 
             this.lblPaterno.AutoSize = true;
@@ -123,15 +136,6 @@
             this.lblNombre.Size = new System.Drawing.Size(44, 13);
             this.lblNombre.TabIndex = 1;
             this.lblNombre.Text = "Nombre";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(6, 26);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 18);
-            this.label1.TabIndex = 0;
             // 
             // panel1
             // 
@@ -171,49 +175,46 @@
             this.btnSiguiente.Text = "Siguiente";
             this.btnSiguiente.UseVisualStyleBackColor = true;
             // 
-            // lblMaterno
+            // label1
             // 
-            this.lblMaterno.AutoSize = true;
-            this.lblMaterno.Location = new System.Drawing.Point(13, 78);
-            this.lblMaterno.Name = "lblMaterno";
-            this.lblMaterno.Size = new System.Drawing.Size(59, 13);
-            this.lblMaterno.TabIndex = 15;
-            this.lblMaterno.Text = "Apellido M.";
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 18);
+            this.label1.TabIndex = 0;
             // 
-            // txtNombre
+            // label3
             // 
-            this.txtNombre.Location = new System.Drawing.Point(139, 23);
-            this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(100, 20);
-            this.txtNombre.TabIndex = 16;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(13, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 15);
+            this.label3.TabIndex = 12;
             // 
-            // txtPaterno
+            // grbOne
             // 
-            this.txtPaterno.Location = new System.Drawing.Point(139, 49);
-            this.txtPaterno.Name = "txtPaterno";
-            this.txtPaterno.Size = new System.Drawing.Size(100, 20);
-            this.txtPaterno.TabIndex = 17;
-            // 
-            // txtMaterno
-            // 
-            this.txtMaterno.Location = new System.Drawing.Point(139, 75);
-            this.txtMaterno.Name = "txtMaterno";
-            this.txtMaterno.Size = new System.Drawing.Size(100, 20);
-            this.txtMaterno.TabIndex = 18;
-            // 
-            // txtTel
-            // 
-            this.txtTel.Location = new System.Drawing.Point(139, 101);
-            this.txtTel.Name = "txtTel";
-            this.txtTel.Size = new System.Drawing.Size(100, 20);
-            this.txtTel.TabIndex = 19;
-            // 
-            // txtMail
-            // 
-            this.txtMail.Location = new System.Drawing.Point(139, 127);
-            this.txtMail.Name = "txtMail";
-            this.txtMail.Size = new System.Drawing.Size(100, 20);
-            this.txtMail.TabIndex = 20;
+            this.grbOne.BackColor = System.Drawing.Color.White;
+            this.grbOne.Controls.Add(this.txtMail);
+            this.grbOne.Controls.Add(this.txtTel);
+            this.grbOne.Controls.Add(this.txtMaterno);
+            this.grbOne.Controls.Add(this.txtPaterno);
+            this.grbOne.Controls.Add(this.txtNombre);
+            this.grbOne.Controls.Add(this.lblMaterno);
+            this.grbOne.Controls.Add(this.lblMail);
+            this.grbOne.Controls.Add(this.lblTelefono);
+            this.grbOne.Controls.Add(this.label3);
+            this.grbOne.Controls.Add(this.lblPaterno);
+            this.grbOne.Controls.Add(this.lblNombre);
+            this.grbOne.Controls.Add(this.label1);
+            this.grbOne.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.grbOne.Location = new System.Drawing.Point(13, 45);
+            this.grbOne.Name = "grbOne";
+            this.grbOne.Size = new System.Drawing.Size(254, 166);
+            this.grbOne.TabIndex = 0;
+            this.grbOne.TabStop = false;
             // 
             // Cliente
             // 
@@ -239,14 +240,11 @@
 
         #endregion
 
-        private System.Windows.Forms.GroupBox grbOne;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label lblTitle;
         private System.Windows.Forms.Label lblNombre;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lblMail;
         private System.Windows.Forms.Label lblTelefono;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label lblPaterno;
         private System.Windows.Forms.Button btnAnterior;
         private System.Windows.Forms.Button btnSiguiente;
@@ -256,5 +254,8 @@
         private System.Windows.Forms.TextBox txtMaterno;
         private System.Windows.Forms.TextBox txtPaterno;
         private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox grbOne;
     }
 }
