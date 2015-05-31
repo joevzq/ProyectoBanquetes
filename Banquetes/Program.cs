028012c765e698c0a65a5ea45a6f1897e3d3ad04
joevzq <joevzq@gmail.com>
2015-05-30 19:51:10 -0500

    Revert "program.cs"
    This reverts commit 9ae1ef3d1a50ffd1febf3d5d1ae3dec6f8bff81b.

--- Banquetes/Program.cs
+++ Banquetes/Program.cs
@@ -16,7 +16,7 @@ namespace Banquetes
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Cliente());
+            Application.Run(new Entrada());
         }
     }
 }
